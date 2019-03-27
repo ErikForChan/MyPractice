5254deb217be9f4650ff2dcecdf87666dd537a63
ErikChan <1043490933@qq.com>
2019-03-27 19:53:23 +0800

    爬虫练习

--- PycharmProjects/Practice/爬虫/redisTest/s4.py
+++ PycharmProjects/Practice/爬虫/redisTest/s4.py
@@ -2,9 +2,9 @@ import redis
 
 pool = redis.ConnectionPool(host='192.168.11.81', port=6379)
 
-conn = redis.Redis(connection_pool=pool)
-# r.set('foo', '友情并阿斯顿发生地方')
-# print(r.get('foo'))
+r = redis.Redis(connection_pool=pool)
+r.set('foo', '友情并阿斯顿发生地方')
+print(r.get('foo'))
 # v = r.getrange('foo',0,3)
 # print(v)
 # v = r.strlen('foo')
@@ -18,13 +18,13 @@ conn = redis.Redis(connection_pool=pool)
 # print(v)
 # aa 0 ba 0 ca 0 da 0 ea 0 fa 0 ga 0
 # 
-conn.zadd('zz', '友情并', -1, '阮国栋', -2,'成汤',-3)
+# conn.zadd('zz', '友情并', -1, '阮国栋', -2,'成汤',-3)
 
 # v = conn.zrange('zz',0,0)
 # print(v[0].decode('utf-8'))
 # print(v[0])
-v = conn.zrank('zz',"成汤")
-print(v)
+# v = conn.zrank('zz',"成汤")
+# print(v)
 
 
 
